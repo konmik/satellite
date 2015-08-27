4f19c317eb68fe89b60c751dfe34aa3459e6ab39
konmik <morelk@gmail.com>
2015-08-27 23:52:29 +0300

    SpaceStation optimized

--- satellite/src/main/java/satellite/connections/SpaceStation.java
+++ satellite/src/main/java/satellite/connections/SpaceStation.java
@@ -2,15 +2,9 @@ package satellite.connections;
 
 import android.util.Printer;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 
-import rx.Notification;
-import rx.Observable;
 import rx.functions.Func0;
-import rx.subjects.Subject;
 import satellite.MissionControlCenter;
 
 /**
@@ -22,7 +16,6 @@ public enum SpaceStation {
 
     INSTANCE;
 
-    private HashMap<String, Subject> earthConnections = new HashMap<>();
     private HashMap<String, Object> raw = new HashMap<>();
 
     <T> T provide(String key, Func0<T> factory) {
@@ -43,30 +36,9 @@ public enum SpaceStation {
         raw.remove(key);
     }
 
-    <T> Observable<Notification<T>> connection(final String key, Func0<Subject> connectionFactory) {
-        if (earthConnections.get(key) == null) {
-            earthConnections.put(key, connectionFactory.call());
-        }
-        return earthConnections.get(key);
-    }
-
-    void clear(Collection<String> keys) {
-        for (String key : keys.toArray(new String[keys.size()])) {
-            raw.remove(key);
-            earthConnections.remove(key);
-        }
-    }
-
     public void print(Printer printer) {
-        printer.println("earth connection keys:");
-        for (String key : earthConnections.keySet())
-            printer.println(key);
         printer.println("raw keys:");
         for (String key : raw.keySet())
             printer.println(key);
-    }
-
-    List<String> getEarthConnectionKeys() {
-        return new ArrayList<>(earthConnections.keySet());
     }
 }
