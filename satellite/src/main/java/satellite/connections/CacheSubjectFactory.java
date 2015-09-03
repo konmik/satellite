271a2d00906e0c2fb45e93798114d0e1912a0cab
konmik <morelk@gmail.com>
2015-09-03 20:39:06 +0300

    subject factory singletons

--- satellite/src/main/java/satellite/connections/CacheSubjectFactory.java
+++ satellite/src/main/java/satellite/connections/CacheSubjectFactory.java
@@ -6,6 +6,13 @@ import rx.subjects.Subject;
 import satellite.SubjectFactory;
 
 public class CacheSubjectFactory<T> implements SubjectFactory<T> {
+
+    private static final CacheSubjectFactory INSTANCE = new CacheSubjectFactory();
+
+    public static <T> CacheSubjectFactory<T> instance() {
+        return INSTANCE;
+    }
+
     @Override
     public Subject<Notification<T>, Notification<T>> call() {
         return BehaviorSubject.create();
--- satellite/src/main/java/satellite/connections/ReplaySubjectFactory.java
+++ satellite/src/main/java/satellite/connections/ReplaySubjectFactory.java
@@ -6,6 +6,13 @@ import rx.subjects.Subject;
 import satellite.SubjectFactory;
 
 public class ReplaySubjectFactory<T> implements SubjectFactory<T> {
+
+    private static final ReplaySubjectFactory INSTANCE = new ReplaySubjectFactory();
+
+    public static <T> ReplaySubjectFactory<T> instance() {
+        return INSTANCE;
+    }
+
     @Override
     public Subject<Notification<T>, Notification<T>> call() {
         return ReplaySubject.create();
