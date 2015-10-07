bfe9faaf6acacf0fb70a05bd3e67af74dcf64090
konmik <morelk@gmail.com>
2015-10-08 00:28:27 +0300

    new tests: RestartableSetTest.test_basic_usage

--- satellite/src/test/java/satellite/RestartableSetTest.java
+++ satellite/src/test/java/satellite/RestartableSetTest.java
@@ -6,126 +6,65 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
 
 import info.android15.satellite.BuildConfig;
 import rx.Notification;
 import rx.Observable;
-import rx.Subscription;
-import rx.observers.TestObserver;
+import rx.observers.TestSubscriber;
+import rx.schedulers.TestScheduler;
 import statemap.StateMap;
 
-import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+import static rx.Observable.interval;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
-public class RestartableSetTest extends BaseRestartableTest {
+public class RestartableSetTest {
 
-    private static class RestartableConnectionSetLauncher implements BaseRestartableTest.InstanceLauncher {
+    public static final int RESTARTABLE_ID = 1;
 
-        final RestartableSet restartableSet;
-        final int restartableId;
+    TestSubscriber<Notification<Long>> subscriber = new TestSubscriber<>();
+    TestScheduler scheduler = new TestScheduler();
 
-        public RestartableConnectionSetLauncher(RestartableSet restartableSet, int restartableId) {
-            this.restartableSet = restartableSet;
-            this.restartableId = restartableId;
-        }
+    @Test
+    public void test_basic_usage() throws Exception {
+        StateMap.Builder builder = StateMap.builder();
+        RestartableSet set = launchRestartable(DeliveryMethod.SINGLE, builder, subscriber, scheduler);
 
-        @Override
-        public Observable<Notification<Integer>> channel(RestartableFactory<String, Integer> restartableFactory) {
-            return restartableSet.restartable(restartableId, DeliveryMethod.LATEST, restartableFactory);
-        }
+        set.launch(1);
+        subscriber.assertNoValues();
+        assertEquals(1, ReconnectableMap.INSTANCE.keys().size());
 
-        @Override
-        public void launch(String arg) {
-            restartableSet.launch(restartableId, arg);
-        }
+        scheduler.advanceTimeBy(1500, TimeUnit.MILLISECONDS);
+        subscriber.assertReceivedOnNext(singletonList(Notification.createOnNext(0L)));
 
-        @Override
-        public void dismiss() {
-            restartableSet.dismiss(restartableId);
-        }
+        scheduler.advanceTimeBy(1000, TimeUnit.MILLISECONDS);
+        subscriber.assertReceivedOnNext(singletonList(Notification.createOnNext(0L)));
+
+        assertEquals(0, ReconnectableMap.INSTANCE.keys().size());
+    }
+
+    private static RestartableSet launchRestartable(DeliveryMethod method, StateMap.Builder builder, TestSubscriber<Notification<Long>> testSubscriber, final TestScheduler scheduler) {
+        RestartableSet set = new RestartableSet(builder);
+        set
+            .restartable(RESTARTABLE_ID, method, new RestartableFactoryNoArg<Long>() {
+                @Override
+                public Observable<Long> call() {
+                    return interval(1, 1, TimeUnit.SECONDS, scheduler);
+                }
+            })
+            .subscribe(testSubscriber);
+        return set;
     }
 
     @After
     public void tearDown() throws Exception {
-        new RestartableTest().tearDown();
-    }
-
-    @Test
-    public void testConnectionImmediate() throws Exception {
-        final RestartableSet restartableSet = new RestartableSet(new StateMap.Builder());
-        testConnectionImmediateStrategy(new RestartableConnectionSetLauncher(restartableSet, 1));
-        testConnectionImmediateStrategy(new RestartableConnectionSetLauncher(restartableSet, 2));
-    }
-
-    @Test
-    public void testConnectionDelayed() throws Exception {
-        final RestartableSet restartableSet = new RestartableSet(new StateMap.Builder());
-        testConnectionDelayedStrategy(new RestartableConnectionSetLauncher(restartableSet, 1));
-        testConnectionDelayedStrategy(new RestartableConnectionSetLauncher(restartableSet, 2));
-    }
-
-    @Test
-    public void testDismiss() throws Exception {
-        final RestartableSet restartableSet = new RestartableSet(new StateMap.Builder());
-        testDismissStrategy(new RestartableConnectionSetLauncher(restartableSet, 1));
-        testDismissStrategy(new RestartableConnectionSetLauncher(restartableSet, 2));
-        testDismissStrategy(new RestartableConnectionSetLauncher(restartableSet, 1));
-        testDismissStrategy(new RestartableConnectionSetLauncher(restartableSet, 1));
-    }
-
-    @Test
-    public void testSaveRestoreRestart() throws Exception {
-        TestObserver<Notification<Integer>> testObserver = new TestObserver<>();
-        TestObserver<Notification<Integer>> testObserver2 = new TestObserver<>();
-
-        StateMap.Builder out = new StateMap.Builder();
-        final RestartableSet base = new RestartableSet(out);
-        Subscription subscription = base.restartable(1, DeliveryMethod.LATEST, INFINITE_RESTARTABLE_FACTORY).subscribe(testObserver);
-        Subscription subscription2 = base.restartable(2, DeliveryMethod.LATEST, INFINITE_RESTARTABLE_FACTORY).subscribe(testObserver2);
-        base.launch(1, "1");
-        base.launch(2, "1");
-
-        testObserver.assertReceivedOnNext(asList(Notification.createOnNext(1)));
-        testObserver2.assertReceivedOnNext(asList(Notification.createOnNext(1)));
-
-        resetStation();
-
-        TestObserver<Notification<Integer>> testObserver21 = new TestObserver<>();
-        TestObserver<Notification<Integer>> testObserver22 = new TestObserver<>();
-        final RestartableSet base2 = new RestartableSet(out.build(), out);
-        Subscription subscription22 = base2.restartable(2, DeliveryMethod.LATEST, INFINITE_RESTARTABLE_FACTORY).subscribe(testObserver22);
-        Subscription subscription21 = base2.restartable(1, DeliveryMethod.LATEST, INFINITE_RESTARTABLE_FACTORY).subscribe(testObserver21);
-
-        testObserver21.assertReceivedOnNext(asList(Notification.createOnNext(1)));
-        testObserver22.assertReceivedOnNext(asList(Notification.createOnNext(1)));
-    }
-
-    @Test
-    public void testSaveRestoreCompleted() throws Exception {
-        TestObserver<Notification<Integer>> testObserver = new TestObserver<>();
-        TestObserver<Notification<Integer>> testObserver2 = new TestObserver<>();
-
-        StateMap.Builder out = new StateMap.Builder();
-        final RestartableSet base = new RestartableSet(out);
-        Subscription subscription = base.restartable(1, DeliveryMethod.LATEST, RESTARTABLE_FACTORY).subscribe(testObserver);
-        Subscription subscription2 = base.restartable(2, DeliveryMethod.LATEST, RESTARTABLE_FACTORY).subscribe(testObserver2);
-        base.launch(1, "1");
-        base.launch(2, "1");
-
-        testObserver.assertReceivedOnNext(asList(Notification.createOnNext(1), Notification.<Integer>createOnCompleted()));
-        testObserver2.assertReceivedOnNext(asList(Notification.createOnNext(1), Notification.<Integer>createOnCompleted()));
-
-        resetStation();
-
-        TestObserver<Notification<Integer>> testObserver21 = new TestObserver<>();
-        TestObserver<Notification<Integer>> testObserver22 = new TestObserver<>();
-        final RestartableSet base2 = new RestartableSet(out.build(), out);
-        Subscription subscription22 = base2.restartable(2, DeliveryMethod.LATEST, INFINITE_RESTARTABLE_FACTORY).subscribe(testObserver22);
-        Subscription subscription21 = base2.restartable(1, DeliveryMethod.LATEST, INFINITE_RESTARTABLE_FACTORY).subscribe(testObserver21);
-
-        testObserver21.assertReceivedOnNext(Collections.<Notification<Integer>>emptyList());
-        testObserver22.assertReceivedOnNext(Collections.<Notification<Integer>>emptyList());
+        synchronized (ReconnectableMap.INSTANCE) {
+            for (String key : new HashSet<>(ReconnectableMap.INSTANCE.keys()))
+                ReconnectableMap.INSTANCE.dismiss(key);
+        }
     }
 }
